0b2d616f84aa4fd5e771c0eb31dac800613a5007
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2019-03-01 14:01:37 -0600

    modulo de fud creado correctamente

--- aplicaciones/fuds/urls.py
+++ aplicaciones/fuds/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from aplicaciones.inicio.views import inicio, err_permisos
-from aplicaciones.fuds.views import MotivoCreate, MotivoUpdate, MotivoList, MotivoDelete
+from aplicaciones.fuds.views import MotivoCreate, MotivoUpdate, MotivoList, MotivoDelete, FudCreate, FudList
 app_name = "fuds"
 urlpatterns = [
     path('NuevoMotivo/', MotivoCreate.as_view(), name='NuevoMotivo'),
@@ -9,4 +9,9 @@ urlpatterns = [
     path('ListarMotivo/', MotivoList.as_view(), name='ListarMotivo'),
     path('EliminarMotivo/<int:pk>/', MotivoDelete.as_view(), name='EliminarMotivo'),
     path('permisos/', err_permisos, name='need_permisos'),
+
+    path('nuevo_fud/', FudCreate.as_view(), name='fud_create'),
+    path('list/fud/', FudList.as_view(), name='fud_list'),
+    path('update/fud/', FudList.as_view(), name='fud_update'),
+    path('delete/fud/', FudList.as_view(), name='fud_delete'),
 ]
