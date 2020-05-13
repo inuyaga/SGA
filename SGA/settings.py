ed1065880ec7f220a400a824920829f9d8824250
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2020-05-13 12:33:08 -0500

    data

--- SGA/settings.py
+++ SGA/settings.py
@@ -102,17 +102,7 @@ DATABASES = {
     }
 }
 
-# PRODUCCION
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'dj_p_sga',
-#         'USER': 'externo',
-#         'PASSWORD': '0102261218',
-#         'HOST': '201.163.99.83',
-#         'PORT': '3307',
-#     }
-# }
+
 
 
 
