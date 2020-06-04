1846187735ba613ed2c5bb6950424084a5a75a4e
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2020-06-04 07:59:40 -0500

    ajax busqueda

--- aplicaciones/pedidos/lookups.py
+++ aplicaciones/pedidos/lookups.py
@@ -1,5 +1,5 @@
 from ajax_select import register, LookupChannel
-from aplicaciones.pedidos.models import Producto
+from aplicaciones.pedidos.models import Producto, Galeria
 
 @register('productos_tags')
 class TagsLookup(LookupChannel):
@@ -35,4 +35,19 @@ class TagsLookupKits(LookupChannel):
 
     def format_item_display(self, item):
         filtering='<span class="tag">{}</span> <p>{}</p> <img src="/media/{}" alt="Producto img" height="42" width="42">'.format(item.producto_codigo,item.producto_nombre, item.producto_imagen)
+        return filtering
+
+
+
+
+@register('galeria_tags')
+class TagsLookup(LookupChannel):
+    model = Galeria
+    def get_query(self, q, request):
+        return self.model.objects.all().order_by('ga_alt')[:100]
+    def format_item_display(self, item):
+        filtering='<img src="{}" alt="{}" style="height: 90px; width: 90px;">'.format(item.ga_foto.url, item.ga_alt)
+        return filtering
+    def format_match(self, obj):
+        filtering='<img src="{}" alt="galeria" style="height: 90px; width: 90px;">'.format(obj.ga_foto.url)
         return filtering
