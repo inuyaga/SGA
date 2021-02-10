997c670c1b74639853e284cb4106a1d66c99f9d6
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2021-02-10 11:14:50 -0600

    cambios en modelos de gasto y reembolso

--- aplicaciones/gasto/models.py
+++ aplicaciones/gasto/models.py
@@ -14,16 +14,15 @@ class TipoGasto(models.Model):
 
 class Gasto(models.Model):
     g_id = models.BigAutoField(primary_key=True, verbose_name="ID")
-    g_tipoGasto = models.ForeignKey(TipoGasto, verbose_name="Tipo de Gasto", on_delete=models.CASCADE)
-    g_monto = models.FloatField(verbose_name="Monto")
-    g_descripcion = models.CharField("Descripcion", max_length=800, help_text="Descripcion del tipo de gasto")
-    g_fechaCreacion = models.DateField(auto_now_add=True, verbose_name="Creado")
+    g_fechaCreacion = models.DateField(auto_now_add=True, verbose_name="Creado")            
     g_estado=models.IntegerField(choices=STATUS, default=1, verbose_name="Estado")
     g_depo = models.ForeignKey(Departamento, on_delete=models.PROTECT, verbose_name="Departamento")
     g_userCreador = models.ForeignKey(Usuario, models.CASCADE, verbose_name="Creó")
-    g_factura = models.FileField(upload_to='gasto/factura/', verbose_name="Factura", help_text="PDF de la factura escaneada")
+    
     def __str__(self):
         return "{}".format(self.g_id)
+    def total_gasto(self):        
+        return self.itemgasto_set.all().aggregate(total_gasto=Sum('itm_monto'))
     class Meta:
         ordering = ['g_estado']
         permissions = (
@@ -33,6 +32,17 @@ class Gasto(models.Model):
             ("puede_rechazar_gasto", "Puede rechazar pago de gastos"),
         )
 
+class ItemGasto(models.Model):
+    itm_gastID=models.ForeignKey(Gasto, on_delete=models.CASCADE, verbose_name="Gasto ID")
+    itm_tipoGasto = models.ForeignKey(TipoGasto, verbose_name="Tipo de Gasto", on_delete=models.PROTECT)
+    itm_monto = models.FloatField(verbose_name="Monto")
+    item_descripcion = models.CharField("Descripcion", max_length=800, help_text="Descripcion del tipo de gasto")
+    itm_factura = models.FileField(upload_to='gasto/factura/', verbose_name="Factura", help_text="PDF de la factura escaneada")
+    itm_ok = models.BooleanField(verbose_name="Validar", default=False)
+    def __str__(self):
+        return self.itm_tipoGasto.nombre
+
+
 
 
 class Reembolso(models.Model):
@@ -41,7 +51,8 @@ class Reembolso(models.Model):
     r_by_user=models.ForeignKey(Usuario, models.CASCADE, verbose_name="Creó")
     r_gastos=models.ManyToManyField(Gasto, verbose_name="Gastos")
     def total(self):        
-        return self.r_gastos.all().aggregate(total_reembolso=Sum('g_monto'))
+        return self.r_gastos.all().aggregate(total_reembolso=Sum('total_gasto'))
+        
     class Meta:
         ordering = ['-r_id']
         # permissions = (
