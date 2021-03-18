88ff76ee79df1221e704384cbc7c8f1c76dcf93e
Gustavo <gustavo_gow1992@hotmail.com>
2021-03-17 21:09:11 -0600

    Actualización de busqueda en sitio web de computel. nuevo diseño y se agrega productos relacionados.

--- aplicaciones/web/templatetags/menu_grid.py
+++ aplicaciones/web/templatetags/menu_grid.py
@@ -1,5 +1,6 @@
 from django import template
-from aplicaciones.pedidos.models import Area, Subcategoria, Linea
+from aplicaciones.pedidos.models import Area, Subcategoria, Linea, Producto
+
 register = template.Library() 
 
 @register.filter
@@ -10,4 +11,9 @@ def get_subcategoria(key):
 @register.filter
 def get_linea(key):
     query = Linea.objects.filter(l_subcat=key)
+    return query
+
+@register.filter
+def get_producto(IDMarca):
+    query = Producto.objects.filter(producto_marca=IDMarca).exclude(producto_marca=40).exclude(producto_importancia = 0).order_by('producto_importancia','producto_marca')[:25]
     return query
