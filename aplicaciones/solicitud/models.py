caeddf3d36ddd669957de7f31a3a3a5a48e6cbb7
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2021-11-13 10:48:48 -0600

    ordenamiento y nuevo status

--- aplicaciones/solicitud/models.py
+++ aplicaciones/solicitud/models.py
@@ -3,11 +3,17 @@ from django.db import models
 from aplicaciones.empresa.models import Empresa, Departamento
 from aplicaciones.inicio.models import User
 
+
 class TipoServicio(models.Model):
     ts_nombre = models.CharField(max_length=250, verbose_name="Nombre")
+
     def __str__(self):
         return self.ts_nombre
-ESTATUS = ((1, 'Creado'), (2, 'Validado'), (3, 'Autorizado'))
+
+
+ESTATUS = ((1, 'Creado'), (2, 'Validado'), (3, 'Autorizado'), (4, 'Cerrado'))
+
+
 class Servicio(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     s_fecha = models.DateField(auto_now_add=True, verbose_name="Creado")
@@ -19,17 +25,18 @@ class Servicio(models.Model):
     s_equipo = models.CharField(max_length=250, verbose_name="Unidad o Equipo")
     s_serie = models.CharField(max_length=250, verbose_name="Numero de Serie")
     s_reporte = models.TextField(verbose_name="Reporte")
-    s_provedor_aut=models.CharField(verbose_name="Provedor Autorizado", max_length=250, blank=True, null=True)
+    s_provedor_aut = models.CharField(verbose_name="Provedor Autorizado", max_length=250, blank=True, null=True)
     s_serv_autorizado = models.CharField(verbose_name="Servicio Autorizado", max_length=200, blank=True, null=True)
     s_presupuesto = models.FloatField(verbose_name="Presupuesto", blank=True, null=True)
-    s_estatus=models.IntegerField(choices=ESTATUS, default=1, verbose_name="Estatus")
-    s_user_cambio = models.ForeignKey(User, related_name="user_cambio", on_delete=models.CASCADE, verbose_name="Modifico")
+    s_estatus = models.IntegerField(choices=ESTATUS, default=1, verbose_name="Estatus")
+    s_user_cambio = models.ForeignKey(User, related_name="user_cambio", on_delete=models.CASCADE,
+                                      verbose_name="Modifico")
+
     def __str__(self):
         return "{}".format(self.id)
+
     class Meta:
         permissions = [
             ('validar_servicio', 'Puede validar el servicio'),
             ('autorizar_servicio', 'Puede autorizar servicio'),
-            ]
-        
-
+        ]
--- aplicaciones/solicitud/views.py
+++ aplicaciones/solicitud/views.py
@@ -1,19 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, ListView, DeleteView, TemplateView
-from aplicaciones.solicitud.models import TipoServicio, Servicio
-from aplicaciones.solicitud.forms import *
-
-from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
-
-from aplicaciones.pago_proveedor.eliminaciones import get_deleted_objects
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from aplicaciones.pago_proveedor.eliminaciones import get_deleted_objects
+from aplicaciones.solicitud.forms import *
+
 
 class TipoServicioCreate(CreateView, PermissionRequiredMixin):
     permission_required = 'solicitud.add_tiposervicio'
@@ -21,10 +16,10 @@ class TipoServicioCreate(CreateView, PermissionRequiredMixin):
     template_name = "solicitud/tipo_servicio_create.html"
     form_class = TipoServicioForm
     success_url = reverse_lazy('solicitud:crear_tipo_servicio')
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tiposervicio_list']=TipoServicio.objects.all()
+        context['tiposervicio_list'] = TipoServicio.objects.all()
         return context
 
 
@@ -36,21 +31,20 @@ class TipoServicioEdit(UpdateView, PermissionRequiredMixin):
     success_url = reverse_lazy('solicitud:crear_tipo_servicio')
 
 
-class TipoServicioDelete(DeleteView, PermissionRequiredMixin): 
+class TipoServicioDelete(DeleteView, PermissionRequiredMixin):
     permission_required = 'solicitud.delete_tiposervicio'
     model = TipoServicio
     template_name = "pedidos/delete_forever.html"
     success_url = reverse_lazy('solicitud:crear_tipo_servicio')
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)   
+        context = super().get_context_data(**kwargs)
 
-        deletable_objects, model_count, protected = get_deleted_objects([self.object])        
+        deletable_objects, model_count, protected = get_deleted_objects([self.object])
         context['deletable_objects'] = deletable_objects
         context['model_count'] = dict(model_count).items()
         context['protected'] = protected
         return context
-    
 
 
 class ServicioList(ListView, PermissionRequiredMixin):
@@ -58,39 +52,43 @@ class ServicioList(ListView, PermissionRequiredMixin):
     model = Servicio
     template_name = "solicitud/solicitud_list.html"
     paginate_by = 50
+
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.order_by('s_estatus')
-        
-        s_fecha=self.request.GET.get('s_fecha')
-        s_fecha2=self.request.GET.get('s_fecha2') 
-        s_tipo=self.request.GET.get('s_tipo')
-        s_empresa=self.request.GET.get('s_empresa')        
-        s_estatus=self.request.GET.get('s_estatus')
+
+        s_fecha = self.request.GET.get('s_fecha')
+        s_fecha2 = self.request.GET.get('s_fecha2')
+        s_tipo = self.request.GET.get('s_tipo')
+        s_empresa = self.request.GET.get('s_empresa')
+        s_estatus = self.request.GET.get('s_estatus')
 
         if s_fecha and s_fecha2:
-            queryset = queryset.filter(s_fecha__range=(s_fecha, s_fecha2))        
+            queryset = queryset.filter(s_fecha__range=(s_fecha, s_fecha2))
         if s_tipo:
-             queryset = queryset.filter(s_tipo=s_tipo)
+            queryset = queryset.filter(s_tipo=s_tipo)
         if s_empresa:
-             queryset = queryset.filter(s_empresa=s_empresa)
+            queryset = queryset.filter(s_empresa=s_empresa)
         if s_estatus:
-             queryset = queryset.filter(s_estatus=s_estatus)
+            queryset = queryset.filter(s_estatus=s_estatus)
+
+        queryset = queryset.order_by('s_fecha')
 
         return queryset
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['forms_filtro']=FiltroServicioForm(self.request.GET)        
+        context['forms_filtro'] = FiltroServicioForm(self.request.GET)
         return context
-    
+
 
 class ServicioCreate(CreateView, PermissionRequiredMixin):
     permission_required = 'solicitud.add_servicio'
     model = Servicio
     template_name = "solicitud/generic_form.html"
     form_class = ServicioForm
-    success_url = reverse_lazy('solicitud:servicios') 
+    success_url = reverse_lazy('solicitud:servicios')
+
     def form_valid(self, form):
         form.instance.s_depo = self.request.user.departamento
         form.instance.s_user = self.request.user
@@ -98,29 +96,31 @@ class ServicioCreate(CreateView, PermissionRequiredMixin):
         self.object = form.save()
         return super().form_valid(form)
 
+
 class ServicioValidarView(UpdateView, PermissionRequiredMixin):
     permission_required = 'solicitud.validar_servicio'
     model = Servicio
     template_name = "solicitud/generic_form.html"
     form_class = ServicioValidarForm
-    success_url = reverse_lazy('solicitud:servicios') 
+    success_url = reverse_lazy('solicitud:servicios')
+
     def form_valid(self, form):
         form.instance.s_estatus = 2
-        form.instance.s_user_cambio = self.request.user        
+        form.instance.s_user_cambio = self.request.user
         self.object = form.save()
         return super().form_valid(form)
 
 
-class ServicioDelete(DeleteView, PermissionRequiredMixin): 
+class ServicioDelete(DeleteView, PermissionRequiredMixin):
     permission_required = 'solicitud.delete_servicio'
     model = Servicio
     template_name = "pedidos/delete_forever.html"
     success_url = reverse_lazy('solicitud:servicios')
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)   
+        context = super().get_context_data(**kwargs)
 
-        deletable_objects, model_count, protected = get_deleted_objects([self.object])        
+        deletable_objects, model_count, protected = get_deleted_objects([self.object])
         context['deletable_objects'] = deletable_objects
         context['model_count'] = dict(model_count).items()
         context['protected'] = protected
@@ -132,12 +132,10 @@ class UpdateStatusServicioView(APIView):
     authentication_classes = ()
 
     def post(self, request, *args, **kwargs):
-        servicio_id = request.data["id"]   
-        user = request.data["changue"]   
-        resp=Servicio.objects.filter(id=servicio_id).update(s_estatus=3, s_user_cambio=user)
+        servicio_id = request.data["id"]
+        user = request.data["changue"]
+        resp = Servicio.objects.filter(id=servicio_id).update(s_estatus=3, s_user_cambio=user)
         data = {
-            'respuesta': resp,        
+            'respuesta': resp,
         }
         return Response(data, status=status.HTTP_202_ACCEPTED)
-
-    
